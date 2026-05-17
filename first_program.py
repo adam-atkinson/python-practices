501c848f40ecb17203c0d1f70c7046076e65b634
Adam <adamatkinson8@icloud.com>
2026-05-17 13:06:46 +0100

    First Program
    I created my first Python file and wrote some code. The famous print("Hello, World!") is the first thing I learned in Python so this had to be added. I then defined some variables including a string, integer, and Boolean value and displayed these values with the print statement

---
+++ first_program.py
@@ -0,0 +1,13 @@
+# Python Practices
+# First Program
+# This is my very first program written in Python
+
+print("Hello, World!")
+
+name = "Adam"
+age = 34
+is_student = True 
+
+print(name)
+print(age)
+print(is_student)
