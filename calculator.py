6b3230ac99d5302b4e06ba3de25732cfdc65ae9e
Adam <adamatkinson8@icloud.com>
2026-05-17 13:19:32 +0100

    Created a calculator
    I wrote a program for a very basic, one operation calculator. The program asks the user for two numbers, which are then converted into integers by using the typecasting method. Python then carries out the operation of adding both numbers together and the print statement displays the result.

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Python Practices
+# Calculator 
+# A basic calculator that asks for user input and adds both numbers together, then displays the reuslt 
+
+num1 = int(input("Enter first number: ")) 
+num2 = int(input("Enter second number: "))
+
+result = num1 + num2
+
+print(result) 
