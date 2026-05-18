f3c20417f03bca3a1118bdbdac2bcd00c3ab9017
Adam <adamatkinson8@icloud.com>
2026-05-18 14:17:54 +0100

    Created Motivational Quotes
    This program uses the random module to select a motivational quote stored in the list and displays it to the user.

---
+++ motivational_quotes.py
@@ -0,0 +1,13 @@
+# Python Practices
+# Random Motivational Quotes
+# This program displays random motivational quotes to the user from the list using the random module
+
+import random 
+
+quotes = ["Keep Going!",
+          "You Can Do This!",
+          "Don't Give Up Now!",
+          "Practice Makes Perfect!",
+          "You Are The Best!"]
+
+print(random.choice(quotes))
